c7026130fedf8cb14501d3bfe5f880aed3711fc4
Tom Wiscombe <wiscombetom@gmail.com>
2022-06-05 15:12:04 -0400

    initial commit

---
+++ events.ts
@@ -0,0 +1,13 @@
+import DiscordJS from "discord.js"
+
+export
+	const onMessageCreate = (message: DiscordJS.Message) => {
+		if (message.content == "ping") {
+			message.reply("PONG!")
+		}
+	}
+
+export
+	const onReady = (client: DiscordJS.Client) => {
+		console.log(`Connected as ${client.user?.tag}`);
+	}
